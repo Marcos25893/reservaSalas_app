8f41e41820b8737e6c9a2d3c3888edfaacb48915
Marcos2593 <mromjim783@g.educaand.es>
2025-12-05 19:12:56 +0100

    Creacion de Api

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SalaResource;
 use App\Models\Reserva;
 use App\Models\Sala;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use function Laravel\Prompts\table;
 
 class ReservaController extends Controller
 {
@@ -59,7 +62,7 @@ class ReservaController extends Controller
         //Lógica para buscar disponibilidad de salas
         // Obtener todas las mesas
         $sala = Sala::where('capacidad', '>=', $numpersonas)
-                        ->where('capacidad', '<=', 2 + ($numpersonas))->get();
+                        ->where('capacidad', '<=', (2 + $numpersonas))->get();
 
         // Filtrar salas ocupadas en esa fecha y hora
         $ocupadas = Reserva::where('fecha', $fecha)
@@ -132,5 +135,116 @@ class ReservaController extends Controller
         }
 
         return redirect()->route('reservas.pendientes');
+    }
+
+
+
+    ////////////////////////////API///////////////////
+    public function apiNewReserva(Request $request){
+        $sala_id=$request->input('sala_id');
+        $telefono=$request->input('telefono');
+        $fecha=$request->input('fecha');
+        $hora=$request->input('hora');
+        $numpersonas=$request->input('numpersonas');
+        $user_id=auth()->user()->id;
+
+        //Comprobar disponibilidad
+        $reservaOcupada = DB::table('reservas')->where('sala_id', $sala_id)
+            ->where('fecha', $fecha)
+            ->where('hora', $hora)
+            ->first();
+
+        if ($reservaOcupada){
+            return response()->json([
+                "mensaje" => "Ya existe una reserva en esta sala",
+                "error" => 1
+            ]);
+        }
+
+        //sala es demasiado grande
+        $sala = Sala::findOrFail($sala_id);
+        if (($sala->capacidad < $numpersonas) || (($sala->capacidad - $numpersonas) > 2)){
+            return response()->json([
+                "mensaje" => "Sala de tamaño incompatible",
+                "error" => 2
+            ]);
+        }
+
+        $reserva = Reserva::create([
+            'sala_id' => $sala_id,
+            'telefono' => $telefono,
+            'fecha' => $fecha,
+            'hora' => $hora,
+            'numpersonas' => $numpersonas,
+            'estado' => 'pendiente',
+            'user_id' => $user_id
+        ]);
+
+        return $reserva->toResource();
+    }
+
+    public function apiUpdateReserva($id){
+        $reservas = Reserva::findOrFail($id);
+
+        if (!auth()->user()->admin){
+            if ($reservas->user_id !=auth()->user()->id){
+                abort(403);
+            }
+        }
+
+        $reservas->estado = 'cancelada';
+        $reservas->save();
+
+        return $reservas->toResource();
+    }
+
+    public function apiDeleteReserva($id){
+        $reservas = Reserva::findOrFail($id);
+
+        //Comprobar que la reserva sea mia o del admin
+        if (!auth()->user()->admin) {
+            if ($reservas->user_id != auth()->user()->id) {
+                abort(403);
+            }
+        }
+
+        $reservas->delete();
+
+        return response()->json([
+            "mensaje" => "Reserva eliminada",
+            "reserva" => $reservas->toResource()
+        ]);
+    }
+
+    public function apiGetSalas(){
+        return SalaResource::collection(Sala::all());
+    }
+
+    public function GetSalaById(string $id){
+        return new SalaResource(Sala::findorFail($id));
+    }
+
+    public function GetReservasAdmin (){
+        //Tienes que ser admin
+        if (auth()->user()->admin) {
+            return Reserva::all()->toResourceCollection();
+        } else
+            abort(401);
+    }
+
+    public  function GetReservasUser(){
+        //Solo muestra tus reservas
+        return Reserva::where('user_id', '=', auth()->id())->get()->toResourceCollection();
+    }
+
+
+    public function getReservasById(string $id){/***///
+        //Muestra una sola reserva por id
+        $reserva = Reserva::findorFail($id);
+
+        if ($reserva->user_id == auth()->id()) {
+            return $reserva->toResource();
+        } else
+            abort(403);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,51 @@
 <?php
 
+use App\Http\Controllers\ReservaController;
 use App\Http\Resources\SalaResource;
+use App\Models\Reserva;
 use App\Models\Sala;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/salas', function (){
-    return SalaResource::collection(Sala::all());
+//Ruta para generar token(No esta protegida)
+Route::post('/login', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
 
+    //Comprobar credenciales
+    if (!Auth::attempt($credentials)) {
+        abort(401);
+    }
+
+    $token = Auth::user()->createToken('my-app-token')->plainTextToken;
+
+    return response()->json(['token' => $token, 'user' => Auth::user()]);
 });
 
-Route::get('/salas/{id}', function (string $id){
-    return new SalaResource(Sala::findorFail($id));
-    //return Sala
 
+//Rutas Protegidas
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/salas', [ReservaController::class, 'apiGetSalas']);
+
+    Route::get('/salas/{id}', [ReservaController::class, 'GetSalaById']);
+
+    Route::get('/reservas/admin', [ReservaController::class, 'GetReservasAdmin']);
+
+    Route::get('/reservas', [ReservaController::class, 'GetReservasUser']);
+
+    Route::get('/reservas/{id}', [ReservaController::class, 'getReservasById']);
+
+    Route::post('/reservas', [ReservaController::class, 'apiNewReserva']);
+
+    Route::put('/reservas/{id}', [ReservaController::class, 'apiUpdateReserva']);
+
+    Route::delete('/reservas/{id}', [ReservaController::class, 'apiDeleteReserva']);
 });
+
